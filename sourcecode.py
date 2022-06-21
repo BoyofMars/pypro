a037da71a669d3846cd5ec43fc48616fee24ed0e
Success Aje <73680210+successaje@users.noreply.github.com>
2022-06-21 14:28:58 -0700

    Update sourcecode.py
    Imported beautifulsoup as bs to make it easier to type as the whole project get bigger and bigger.

--- sourcecode.py
+++ sourcecode.py
@@ -1,6 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 url= "https://w3schools.com/python/demopage.htm"
 page=requests.get(url)
-soup=BeautifulSoup(page.content, "html.parser")
+soup=bs(page.content, "html.parser")
 print(soup.prettify())
